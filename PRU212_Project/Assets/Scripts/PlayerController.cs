eeaaddeadaee975cef9e62949856231d2eb7c32e
annnhde180150 <annnhde180150@fpt.edu.vn>
2025-02-17 16:52:54 +0700

    Add animation

--- PRU212_Project/Assets/Scripts/PlayerController.cs
+++ PRU212_Project/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    
+    [Header("Animation")]
+    [SerializeField] public Animator animator;
+
     [Header("WallCheck")]
     [SerializeField] public Transform wallCheckpos;  
+
     [SerializeField] public LayerMask wallLayer;
 
     [Header("WallMovement")]
@@ -39,6 +42,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private float dashPower = 100f;
     [SerializeField] private float dashTime = 0.2f;
     [SerializeField] private float dashCooldown = 1f;
+    [SerializeField] private TrailRenderer tr;
     private float dashCountdown = 0;
     private int dashCount = 0;
     private bool isDashing = false;
@@ -64,16 +68,19 @@ public class NewMonoBehaviourScript : MonoBehaviour
         if (!isDashing)
         {
             Move();
-            Jump();
+            Jump();           
+            CheckGrounded();
             wallSlide();
             WallJump();
             StartCoroutine(Dash());
+            
         }
     }
 
     //movement function
     private void Move()
-    {       
+    {
+        
         float x = Input.GetAxis("Horizontal");
         if (x * direction < 0)
         {           
@@ -81,16 +88,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
             transform.Rotate(0, 180, 0);
         }
         if (isGrounded)
-        {
+        {            
+            animator.SetFloat("speed", Mathf.Abs(x));
             rb.linearVelocity = new Vector2(x * moveSpeed, rb.linearVelocity.y);
         }else if(!isGrounded && !isWallSliding && x != 0)
         {
             rb.AddForce(new Vector2(airMovementSpeed* x,0),ForceMode2D.Force);            
-            rb.linearVelocity = new Vector2(Mathf.Clamp(rb.linearVelocity.x, -moveSpeed, moveSpeed), rb.linearVelocity.y);
-            //if(Mathf.Abs(rb.linearVelocityX) > moveSpeed)
-            //{
-            //    rb.linearVelocity = new Vector2(x * moveSpeed, rb.linearVelocity.y);
-            //}
+            rb.linearVelocity = new Vector2(Mathf.Clamp(rb.linearVelocity.x, -moveSpeed, moveSpeed), rb.linearVelocity.y);           
         }
 
        
@@ -100,35 +104,42 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void Jump()
     {
-        if ((Input.GetButtonDown("Jump") || Input.GetButtonDown("Vertical")) && JumpCount++ < maxJump-1 && !isWallSliding)
+        if ((Input.GetButtonDown("Jump") || Input.GetButtonDown("Vertical")) && JumpCount++ < maxJump - 1 && !isWallSliding)
         {
             Debug.Log("Jump");
+            animator.SetBool("IsJump",true);
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
-        
-        
-        if(isGrounded)
-        {
             
-            JumpCount = 0;           
         }       
-    }    
+    } 
+    private void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        if (isGrounded)
+        {
+            animator.SetBool("IsJump", false);
+            JumpCount = 0;
+        }
+    }
+
     private void wallSlide()
     {
         isTouchingWall = Physics2D.OverlapCircle(wallCheckpos.position, 0.1f, wallLayer);
 
         if (!isGrounded && isTouchingWall && rb.linearVelocityY < 0)
-        {        
+        {
+            animator.SetBool("IsWallAni", true);
             isWallSliding = true;                     
         }
         else
-        {            
+        {
+            animator.SetBool("IsWallAni", false);
             isWallSliding = false;
         }
 
         if (isWallSliding)
         {
+            
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, -wallSlideSpeed);
         }
     }
@@ -150,7 +161,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {
             Debug.Log("WallJump");
             IswallJumping = true;
-            
+            animator.SetBool("IsJump", true);
             rb.linearVelocity = new Vector2(wallJumpingForce.x * -direction, wallJumpingForce.y);           
             Invoke(nameof(StopWallJumping), wallJumpingDuration);
         }
@@ -167,9 +178,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
             float originalGravity = rb.gravityScale;
             rb.gravityScale = 0;
             rb.linearVelocity = new Vector2(dashPower*direction , 0);
+            tr.emitting = true;
+            animator.SetBool("IsRoll", true);
+            animator.SetBool("IsJump", false);
             yield return new WaitForSeconds(dashTime);
             rb.gravityScale = originalGravity;
+            animator.SetBool("IsJump", true);
+            animator.SetBool("IsRoll", false);
             isDashing = false;
+            tr.emitting = false;
         }
         if(isGrounded || isTouchingWall) dashCount = 0;
     } 
