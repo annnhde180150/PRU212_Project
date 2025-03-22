e8e20ce03f5b8c1fed7f03f6075ed34e47d9588f
hoangphuongphan <166013675+hoangphuongphan@users.noreply.github.com>
2025-03-22 16:10:48 +0700

    add more move to pattern

--- PRU212_Project/Assets/Scripts/BossEnemy.cs
+++ PRU212_Project/Assets/Scripts/BossEnemy.cs
@@ -8,43 +8,61 @@ using UnityEngine.UIElements;
 public class BossEnemy : Enemy
 {
     [Header("Attack")]
+    [SerializeField] private Transform _firePoint;
     [SerializeField] protected GameObject bulletPrefab;
     protected bool isShooting = false;
 
-    [SerializeField] private Transform firePoint;
+    [SerializeField] private float laserTime = 4f;
+    [SerializeField] private TrailRenderer tr;
     public bool isImmuned = false;
     private bool isPatternAttacking = false;
     public bool isMelee = false;
     private bool canMove = true;
+    private bool canStop = true;
     AnimatorStateInfo stateInfo;
+    private float offsetX;
+    private float offsetY;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         direction = 1;
+        offsetX = _firePoint.position.x - transform.position.x;
+        offsetY = _firePoint.position.y - transform.position.y;
+        tr.emitting = false;
+        tr.enabled = true;
+        isDead = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        checkRange();
-        if (isRangeReached && canTurn)
+        if (!isDead)
         {
-            direction = flip();
-            canTurn = false;
-        }
-        if (!isRangeReached)
-        {
-            canTurn = true;
-        }
-        if (!isPatternAttacking)
-        {
-            isPatternAttacking = true;
-            StartCoroutine(PatternAttacking());
-        }
-        if (canMove) Move(direction);
-        else Stop();
+            checkRange();
+            if (isRangeReached && canTurn)
+            {
+                direction = flip();
+                canTurn = false;
+            }
+            if (!isRangeReached)
+            {
+                canTurn = true;
+            }
+            if (!isPatternAttacking)
+            {
+                isPatternAttacking = true;
+                StartCoroutine(PatternAttacking());
+            }
+            if (health <= 0)
+            {
+                StartCoroutine(Die());
+            }
+            if (canMove) Move(direction);
+            else if (canStop) Stop();
+        }else StartCoroutine(Die());
     }
 
+    //work
     IEnumerator Shielding()
     {
         canMove = false;
@@ -60,11 +78,43 @@ public class BossEnemy : Enemy
         canMove = true;
     }
 
-    void Enhance()
+    //work
+    IEnumerator Rolling()
     {
-        animation.SetBool("isEnhancing", true);
+        canMove = false;
+        animation.SetBool("isImmune", true);
+        yield return new WaitForSeconds(0.1f);
+
+        // animation
+        stateInfo = animation.GetCurrentAnimatorStateInfo(0);
+        yield return new WaitForSeconds(stateInfo.length);
+        yield return new WaitForSeconds(0.22f);
+
+        //rolling
+        var playPos = GameObject.Find("Player").transform.position;
+        var rollDirection = playPos.x > transform.position.x ? 1 : -1;
+        if (direction != rollDirection)
+        {
+            direction = flip();
+        }
+        animation.speed = 0;
+        yield return new WaitForSeconds(1f);
+        canStop = false;
+        tr.emitting = true;
+        rb.linearVelocity = new Vector2(direction * speed * 10f, rb.linearVelocity.y);
+        yield return new WaitForSeconds(1f);
+        tr.emitting = false;
+
+
+        //end rolling
+        canStop = true;
+        animation.SetBool("isImmune", false);
+        yield return null;
+        animation.speed = 1;
+        canMove = true;
     }
 
+    //work partly
     IEnumerator ShootArm()
     {
         //start shooting
@@ -79,19 +129,21 @@ public class BossEnemy : Enemy
         
         //render bullet
         var playPos = GameObject.Find("Player").transform.position;
-        Debug.Log(playPos);
-        var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        bullet.GetComponent<Bullet>().SetTarget(playPos);
+        var newFirePoint = new Vector3(_firePoint.position.x + offsetX, _firePoint.position.y + offsetY - 1, 0);
+        var bullet = Instantiate(bulletPrefab, newFirePoint, _firePoint.rotation);
+        bullet.GetComponent<Bullet>().SetTarget(playPos, _firePoint.position);
 
         //end shooting
         animation.SetBool("isShooting", false);
         canMove = true;
     }
 
+    //work
     IEnumerator meleeAttack()
     {
         canMove = false;
-        animation.SetBool("isAttacking", true);
+        isMelee = true;
+        animation.SetBool("isMeleeAttack", true);
         yield return null;
 
         // Continuously update stateInfo inside the loop
@@ -99,13 +151,67 @@ public class BossEnemy : Enemy
         yield return new WaitForSeconds(stateInfo.length);
         yield return new WaitForSeconds(0.1f);
 
-        animation.SetBool("isAttacking",false);
+        animation.SetBool("isMeleeAttack", false);
+        isMelee = false;
         canMove = true;
+    }
+
+    //work
+    IEnumerator Stun()
+    {
+        canMove = false;
+        animation.SetBool("isStunt", true);
+        isImmuned = false;
+        yield return null;
+
+        // Continuously update stateInfo inside the loop
+        //stateInfo = animation.GetCurrentAnimatorStateInfo(0);
+        //yield return new WaitForSeconds(stateInfo.length);
+        yield return new WaitForSeconds(StuntTime);
+
+        animation.SetBool("isStunt", false);
+        canMove = true;
+    }
+
+    IEnumerator Lasering()
+    {
+        canMove = false;
+        animation.SetBool("IsLaser", true);
+        yield return null;
+
+        yield return new WaitForSeconds(laserTime);
+
+        animation.SetBool("IsLaser", false);
+        canMove = true;
+    }
+
+    //not work
+    IEnumerator Die()
+    {
+        if (isDying) yield break;
+        isDying = true;
+        canMove = false;
+        animation.SetBool("isDead", true);
+        yield return null;
+
+        rb.gravityScale = 0;
+        rb.linearVelocity = Vector2.zero;
+
+        GetComponent<Collider2D>().enabled = false;
+        foreach (Collider2D c in GetComponentsInChildren<Collider2D>())
+        {
+            c.enabled = false;
+        }
+
+        stateInfo = animation.GetCurrentAnimatorStateInfo(0);
+        yield return new WaitForSeconds(stateInfo.length);
+        yield return new WaitForSeconds(0.5f);
+        animation.speed = 0;
     }
 
     private void Stop()
     {
-        rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(0, 0);
     }
 
     IEnumerator PatternAttacking()
@@ -114,15 +220,20 @@ public class BossEnemy : Enemy
         yield return new WaitForSeconds(2.0f);
 
         yield return StartCoroutine(ShootArm());
-        //yield return StartCoroutine(ShootArm());
-        //yield return StartCoroutine(ShootArm());
+        yield return StartCoroutine(ShootArm());
+        yield return StartCoroutine(ShootArm());
         yield return new WaitForSeconds(2.0f);
 
         yield return StartCoroutine(meleeAttack());
         yield return new WaitForSeconds(2.0f);
 
+        yield return StartCoroutine(Lasering());
+        yield return StartCoroutine(Rolling());
+        yield return StartCoroutine(Stun());
+        yield return new WaitForSeconds(2.0f);
+
         //finish pattern
-        yield return new WaitForSeconds(5.0f);
+        //yield return new WaitForSeconds(5.0f);
         canMove = true;
         isPatternAttacking = false;
     }
--- PRU212_Project/Assets/Scripts/Bullet.cs
+++ PRU212_Project/Assets/Scripts/Bullet.cs
@@ -4,34 +4,29 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] Material blast;
     [SerializeField] public float speed = 10f;
-    public Vector3 _target;
     private Vector3 direction; // Direction to the target
     private float angle; // Angle to rotate the bullet
 
-    public void SetTarget(Vector2 target)
+    public void SetTarget(Vector3 target, Vector3 firePoint)
     {
-        Debug.Log("Bullet Position: " + transform.position);
-        Debug.Log("Target Position: " + target);
-        _target = target;
 
-        direction = (_target - transform.position).normalized;
-        Debug.Log("Direction: " + direction);
+        direction = (target - firePoint).normalized;
 
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Debug.Log("Angle: " + angle);
 
         // Set the rotation once
         transform.rotation = Quaternion.Euler(0, 0, angle);
+        speed *= firePoint.x > target.x ? 1 : -1;
     }
 
     void Update()
     {
         // Move the bullet in the pre-calculated direction
-        transform.Translate(direction * speed * Time.deltaTime);
+        GetComponent<Rigidbody2D>().linearVelocity = direction * speed;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject, 1f);
+        Destroy(gameObject);
     }
 }
